9872933710dc104993c95cbe0bcdec254e7991b6
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-16 09:45:21 +0100

    Create ASCII prethodnik ili sledbenik.c

---
+++ Casovi Vezbi/Tema 5/ASCII prethodnik ili sledbenik.c
@@ -0,0 +1,14 @@
+//Napisati program koji učitava karakter c i ispisuje:
+//karakter koji sledi nakon karaktera c u ASCII tabeli ukoliko je njegov ASCII kod paran
+//karakter koji prethodi karakteru c u ASCII tabeli, inače
+//Napomena: Prethodnik prvog karaktera je poslednji karakter ASCII tabele, a sledbenik poslednjeg, prvi karakter ASCII tabele.
+#include <stdio.h>
+
+int main(){
+    char c;
+    scanf("%c", &c);
+    if(c%2 == 0)
+        printf("%c\n", c+1);
+    else
+        printf("%c\n",c-1);
+}
