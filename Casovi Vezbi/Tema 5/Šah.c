09ec5c13152770781632f8901d9f0b336bfbf1e3
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-16 09:48:19 +0100

    Create Šah.c

---
+++ Casovi Vezbi/Tema 5/Šah.c
@@ -0,0 +1,49 @@
+//Polje šahovske table se definiše parom celih brojeva (x,y), 1 <= x,y <= 8, gde je x redni broj reda, a y redni broj kolone.
+//Ako su dati takvi parovi, napisati program koji proverava:
+//da li su polja (k, l) i (m, n) iste boje
+//da li kraljica sa (k, l) ugrožava polje (m, n)
+//da li konj sa (k, l) ugrožava polje (m, n)
+//Pretpostaviti da je polje (1,1) crno i da predstavlja donji levi ugao šahovske table. Sa ulaza se učitavaju 4 cela broja
+//k, l, m i n. Na izlaz ispisati odgovarajuće poruke (pogledati test primere). U slučaju neispravnog unosa, ispisati -1 i prekinuti izvršavanje programa.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int k,l,m,n;
+    scanf("%d %d %d %d", &k,&l,&m,&n);
+    int raz1 = k - m;
+    int raz2 = l-n;
+    if (k < 1 || k > 8 || l < 1 || l > 8 || m < 1 || m > 8 || n < 1 || n > 8){
+        puts("-1");
+        return 1;
+    }
+    if (k == m && l == n){
+        puts("-1");
+        return 1;
+    }
+    if (k%2 == m%2){
+        if(l%2 == n%2){
+            printf("Polja su iste boje.\n");
+        }
+        else
+            printf("Polja su razlicite boje.\n");
+    }
+    else
+        if(l%2 == n%2)
+            printf("Polja su razlicite boje.\n");
+        else
+            printf("Polja su iste boje.\n");
+    if (k == m)
+        printf("Kraljica sa (%d,%d) ugrozava polje (%d,%d).\n", k,l,m,n);
+    else if (l == n)
+        printf("Kraljica sa (%d,%d) ugrozava polje (%d,%d).\n",k,l,m,n);
+        else if (abs(raz1) == abs(raz2))
+            printf("Kraljica sa (%d,%d) ugrozava polje (%d,%d).\n",k,l,m,n);
+            else
+                printf("Kraljica sa (%d,%d) ne ugrozava polje (%d,%d).\n",k,l,m,n);
+    if (k == m+1 && l == n+2 || k == m-1 && l == n+2 || k == m+1 && l == n-2 || k == m-1 && l == n-2 || k == m+2 && l == n+1 || k == m-2 && l == n+1 || k == m+2 && l == n-1 || k == m-2 && l == n-1)
+        printf("Konj sa (%d,%d) ugrozava polje (%d,%d).\n",k,l,m,n);
+    else
+        printf("Konj sa (%d,%d) ne ugrozava polje (%d,%d).\n",k,l,m,n);
+    return 0;
+}
