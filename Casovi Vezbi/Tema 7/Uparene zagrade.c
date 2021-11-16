96dbc7172402c5b73b6241005344e913d6532e66
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-16 09:59:36 +0100

    Create Uparene zagrade.c

---
+++ Casovi Vezbi/Tema 7/Uparene zagrade.c
@@ -0,0 +1,21 @@
+//Napisati program koji ucitava karaktere do kraja ulaza. Program proverava da li su uglaste zagrade ispravno uparene i ako jesu ispisuje 1
+//a u suprotnom ispisuje 0 na standardni izlaz.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char c;
+    int br = 0;
+    while((c = getchar()) && c != EOF && c !='\n'){
+        if(c == '[')
+            br++;
+        if(c == ']'){
+            br--;
+            if(br<0){
+                puts("0");
+                return 0;
+            }
+        }
+    }
+    puts((br == 0)? "1": "0");
+}
