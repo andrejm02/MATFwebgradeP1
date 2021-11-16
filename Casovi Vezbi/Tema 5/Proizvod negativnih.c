c60629045687c2953374998284cc97242f9a8335
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-16 09:31:55 +0100

    Create Proizvod negativnih.c

---
+++ Casovi Vezbi/Tema 5/Proizvod negativnih.c
@@ -0,0 +1,17 @@
+//Napisati program koji učitava četiri cela broja i ispisuje proizvod negativnih.
+//U slučaju da nijedan od učitanih brojeva nije negativan, ispisati 1.
+#include <stdio.h>
+
+int main(){
+    int a,b,c,d,broj = 1;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    if (a < 0)
+        broj = broj * a;
+    if (b < 0)
+        broj = broj * b;
+    if (c < 0)
+        broj = broj * c;
+    if (d < 0)
+        broj = broj *d;
+    printf("%d\n", broj);
+}
