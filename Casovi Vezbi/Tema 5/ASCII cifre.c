f541cb2f297da715a073fae9d3becbb33331e12d
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-16 09:32:54 +0100

    Create ASCII cifre.c

---
+++ Casovi Vezbi/Tema 5/ASCII cifre.c
@@ -0,0 +1,22 @@
+//Napisati program učitava pet karaktera i ispisuje sumu ASCII kodova učitanih karaktera koji su cifre.
+//U slučaju da nijedan od karaktera nije cifra, ispisati 0.
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    char a,b,c,d,e;
+    int suma = 0;
+    scanf("%c%c%c%c%c", &a, &b ,&c, &d, &e);
+
+    if(isdigit(a))
+        suma += a;
+    if(isdigit(b))
+        suma += b;
+    if(isdigit(c))
+        suma += c;
+    if(isdigit(d))
+        suma += d;
+    if(isdigit(e))
+        suma += e;
+    printf("%d", suma);
+}
