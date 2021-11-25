63a873963e06e779d26479f24e46079d34fee933
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-25 16:11:51 +0100

    Delete CasoviVezbi/Tema3 directory

--- CasoviVezbi/Tema3/AritmetickaSredina.c
+++
@@ -1,9 +0,0 @@
-//Napisati program koji za dva uneta broja ispisuje njihovu aritmeticku sredinu (zaokruzenu na dve decimale).
-#include <stdio.h>
-
-int main(){   
-    float a,b,as;
-    scanf("%f %f", &a, &b);
-    as = (a+b)/2;
-    printf("%.2f", as);
-}
