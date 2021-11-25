2d3a78257aa6114f0a7b73f672a5b54ed7c7661a
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-25 13:41:19 +0100

    Create Konverzija.c

---
+++ Casovi Vezbi/Tema8/Konverzija.c
@@ -0,0 +1,16 @@
+//Napisati funkciju int konverzija(char x) koja vraća poslednju cifru ASCII koda karaktera x. Napisati program koji učitava karaktere sa standardnog ulaza
+//sve dok korisnik ne unese EOF i za svaki učitan karakter ispisuje povratnu vrednost funkcije konverzija.
+#include <stdio.h>
+
+int konverzija(char x){
+    int n = x;
+    printf("%d", n%10);
+    return 0;
+}
+
+int main(){
+    char x;
+    while(scanf("%c", &x) != EOF)
+        konverzija(x);
+    return 0;
+}
