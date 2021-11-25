582c64df86e2bf7ef6c1aeb0a050e193f724c893
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-25 21:51:25 +0100

    Create DNK.c

---
+++ Casovi Vezbi/Tema10/DNK.c
@@ -0,0 +1,23 @@
+//Napisati program koji racuna broj mutacija u dnk. Sa standardnog ulaza ucitavaju se dve niske (prvi i drugi lanac), duzine najvise 100, a na standardi
+//izlaz ispisuje se broj mutacija.
+//Uputstvo: Dnk lanci treba da budu komplementarni. Lanci su komplementarni ukoliko na istim pozijima u niskama nalaze komplementarni nukleotidi. 
+//Komplementarni su nukleotidi A i T, kao i nukleotidi C i G. Svaka pozicija na kojoj se ne nalaze komplementarni nukleotidi predstavlja jednu mutaciju.
+#include <stdio.h>
+#include <string.h>
+#define MAKS 100
+
+void compare(char s[], char d[]){
+    int br = 0;
+    for(int i = 0; i < strlen(s); i++){
+        if(!((s[i] == 'a' && d[i] == 't') || (s[i] == 't' && d[i] == 'a') || (s[i] == 'g' && d[i] == 'c') || (s[i] == 'c' && d[i] == 'g'))){
+            br++;
+        }
+    }
+    printf("%d\n", br);
+}
+
+int main(){
+    char a[MAKS], b[MAKS];
+    scanf("%s%s",a,b);
+    compare(a,b);
+}
