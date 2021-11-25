209e1d406fac66bd0106e2289c2f546a4d898855
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2021-11-25 13:40:27 +0100

    Create Prebrojavanje.c

---
+++ Casovi Vezbi/Tema8/Prebrojavanje.c
@@ -0,0 +1,18 @@
+//Napisati funkciju int prebroj_manje(int x) koja učitava cele brojeve sa standardnog ulaza sve dok korisnik ne unese EOF i prebrojava koliko puta se pojavio
+//broj manji od x. Napisati program koji prvo učitava ceo broj x, a zatim testira rad napisane funkcije.
+#include <stdio.h>
+
+int prebrojavanje(int x){
+    int n,br = 0;
+    while(scanf("%d", &n) != EOF)
+        if(x > n)
+            br++;
+    printf("%d",br);
+    return 0;
+}
+
+int main(){
+    int x;
+    scanf("%d", &x);
+    prebrojavanje(x);
+}
