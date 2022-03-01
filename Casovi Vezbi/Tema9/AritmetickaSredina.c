350e529c6bc898de1563cfcf3ebcbe94ef4d26cc
andrej <57645580+andrejm02@users.noreply.github.com>
2022-03-01 19:03:51 +0100

    Update AritmetickaSredina.c

--- Casovi Vezbi/Tema9/AritmetickaSredina.c
+++ Casovi Vezbi/Tema9/AritmetickaSredina.c
@@ -1,27 +1,29 @@
-//Sa standardnog ulaza unosi se broj n, a zatim i n elemenata niza celih brojeva (niz ne sme biti imati vise od 100 elemenata). 
-//Napisati program koji na standardni izlaz, na sa tacnoscu od dve decimale ispisuje zbir aritmeticke sredine elemenata sa parnih pozicija i 
+//Sa standardnog ulaza unosi se broj n, a zatim i n elemenata niza celih brojeva (niz ne sme imati vise od 100 elemenata). 
+//Napisati program koji na standardni izlaz, sa tacnoscu od dve decimale ispisuje zbir aritmeticke sredine elemenata sa parnih pozicija i 
 //kvadratne sredine elemenata sa neparnih pozicija (koren aritmeticke sredine kvadrata). 
 //U slucaju neispravnog ulaza (n<2 ili n>100) na izlaz ispisati -1 i prekinuti program.
 #include <stdio.h>
 #include <math.h>
 
 int main(){
-    int x,i,br1 = 0,br2 = 0; float as = 0,ks = 0;
-    scanf("%d", &x);
-    int n[x];
-    if(x < 2 || x > 100){
+    int n,i,br1 = 0,br2 = 0; float as = 0,ks = 0;
+    scanf("%d", &n);
+    int a[n];
+    
+    if(n < 2 || n > 100){
         puts("-1");
         return 1;
     }
-    for(i = 0; i < x; i++)
-        scanf("%d", &n[i]);
-    for(i = 0; i< x; i++)
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    
+    for(i = 0; i< n; i++)
         if(i%2 == 0){
-            as += n[i];
+            as += a[i];
             br1++;
         }
         else if(i%2 == 1){
-            ks += n[i]*n[i];
+            ks += a[i]*a[i];
             br2++;
         }
     printf("%.2f\n", as/br1 + sqrt(ks/br2));
