c3db0269b378f81c80ef56d3b40f94e8a90393b7
andrej <57645580+andrejm02@users.noreply.github.com>
2022-03-01 17:09:54 +0100

    Update ASCIIcifre.c

--- Casovi Vezbi/Tema 5/ASCIIcifre.c
+++ Casovi Vezbi/Tema 5/ASCIIcifre.c
@@ -10,13 +10,18 @@ int main(){
 
     if(isdigit(a))
         suma += a;
+    
     if(isdigit(b))
         suma += b;
+    
     if(isdigit(c))
         suma += c;
+    
     if(isdigit(d))
         suma += d;
+    
     if(isdigit(e))
         suma += e;
+    
     printf("%d", suma);
 }
