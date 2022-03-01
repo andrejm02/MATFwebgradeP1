a5f8659a08652fd7554d1c8884f5b24b7abebabe
andrej <57645580+andrejm02@users.noreply.github.com>
2022-03-01 18:33:49 +0100

    Update Talas.c

--- Casovi Vezbi/Tema 6/Talas.c
+++ Casovi Vezbi/Tema 6/Talas.c
@@ -2,37 +2,34 @@
 //Niz brojeva predstavlja talas ako vazi a1 < a2 > a3 < a4 > a5 < ... ili a1 > a2 < a3 > a4 < a5 > ... U slučaju greške ispisati -1 i prekinuti program.
 #include <stdio.h>
 int main() {
-    int x,copy1 = -1000, c, d, n = 0, m = 0;
-    scanf("%d", &x);
-    if(x < 0){
+    int n,copy1 = -1000, broj, y = 0, x = 0;
+    //x je brojac za sve brojeve koji su veci od prethodnog, y je brojac za sve brojeve koji su manji od prethodnog
+    scanf("%d", &n);
+    
+    if(n < 0){
         puts("-1");
         return 1;
+    }
+    for(int i = 0; i < n; i++){
+        scanf("%d", &broj);
+
+        if(broj > copy1 && i != 0){
+            x++;
+            y = 0;
+        }else if (broj < copy1 && i != 0){
+            y++;
+            x = 0;
         }
-    for(int i = 0; i < x; i++){
-        scanf("%d", &c);
-        if(c > copy1 && i != 0){
-            n++;
-            m = 0;
-            if(n == 2){
-                puts("0");
-                return 1;
-            }
-        }
-        else if (c < copy1 && i != 0){
-            m++;
-            n = 0;
-            if(m == 2){
-                puts("0");
-                return 1;
-            }
-        }
-        copy1 = c;
-    }   
-        if(m == n+1|| n == m+1)
-                puts("1");
-        else if(x == 1 && m == 0 && n == 0)
-            puts("1");
-        else
+        if(x == 2 || y == 2){
             puts("0");
-        return 0;
+            return 1;
+        }
+        copy1 = broj;
+    }   
+    if(y == x+1|| x == y+1 || (n == 1 && y == 0 && x == 0))
+        puts("1");
+    else
+        puts("0");
+
+    return 0;
 }
