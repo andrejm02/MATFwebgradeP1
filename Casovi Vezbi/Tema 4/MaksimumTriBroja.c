1a800e8cebea87054e82fd3d575d2a345075f4a8
andrej <57645580+andrejm02@users.noreply.github.com>
2022-03-01 17:05:16 +0100

    Update MaksimumTriBroja.c

--- Casovi Vezbi/Tema 4/MaksimumTriBroja.c
+++ Casovi Vezbi/Tema 4/MaksimumTriBroja.c
@@ -4,15 +4,17 @@
 int main(){
     int a,b,c,max;
     scanf("%d %d %d", &a, &b, &c);
-    if (a > b)
+    
+    if (a > b){
         if (a > c)
             max = a;
         else
             max = c;
-    else
+    }else{
         if (b > c)
             max = b;
         else
             max = c;
+    }
     printf("%d", max);
 }
